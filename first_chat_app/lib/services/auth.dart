efa7810bde2a76eabe7c992836a06aaa71a6292b
Phil <phil.dlebel.eco@gmail.com>
2020-08-12 13:20:04 -0400

    3.3

--- first_chat_app/lib/services/auth.dart
+++ first_chat_app/lib/services/auth.dart
@@ -13,7 +13,7 @@ class AuthMethods{
     try{
       AuthResult result = await _auth.signInWithEmailAndPassword(email: email, password: password);
       FirebaseUser firebaseUser = result.user;
-      return _userfromFirebaseUser(firebaseUser)
+      return _userfromFirebaseUser(firebaseUser);
 
 
     }catch(e){
@@ -29,6 +29,22 @@ class AuthMethods{
       return _userfromFirebaseUser(firebaseUser);
 
 
+    }catch(e){
+      print(e.toString());
+    }
+  }
+
+  Future resetPass(String email)async{
+    try{
+      return await _auth.sendPasswordResetEmail(email: email);
+    }catch(e){
+      print(e.toString());
+    }
+  }
+
+  Future signOut() async {
+    try{
+      return await _auth.signOut();
     }catch(e){
       print(e.toString());
     }
